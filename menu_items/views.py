231c61bb2c54303bb86173601cad580d0be52530
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-07-27 14:39:45 +0000

    update

--- menu_items/views.py
+++ menu_items/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
 # Create your views here.
+
+def display_menu_items(request):
+    html = models.menu_items
+    return HttpResponse(html)
